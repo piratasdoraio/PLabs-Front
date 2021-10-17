0d34312a9e8bab02b0c2355fc32541e68a3516fb
Marco <raiomarco@piratasdoraio.com>
2021-10-17 03:37:42 -0300

    Agora tem quadros

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage';
 import Home from './pages/HomePage';
+import Quadros from './pages/Quadros';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Home}>
-          </Route>
-          <Route path="/login" component={LoginPage}>
-          </Route>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/quadros" component={Quadros} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ src/pages/Quadros.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Board from 'react-trello';
+
+const data = {
+  lanes: [
+    {
+      id: 'Coluna1',
+      title: 'Fase 1',
+      label: '2/2',
+      cards: [
+        {
+          id: 'Card1',
+          title: 'Jose Cleitin',
+          description: 'Vendedor de fruta',
+          label: 'Alta prioridade',
+          draggable: false,
+        },
+        {
+          id: 'Card2',
+          title: 'Roberto',
+          description: 'Designer',
+          label: 'Deus',
+          metadata: { sha: 'be312a1' },
+        },
+      ],
+    },
+    {
+      id: 'Coluna2',
+      title: 'Fase2',
+      label: '0/0',
+      cards: [],
+    },
+  ],
+};
+
+export default function quadros() {
+  return (
+    <>
+      <Board data={data} />
+    </>
+  );
+}
