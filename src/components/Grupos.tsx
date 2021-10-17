648022048d80504df9826fd9f2dcee589062db37
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2021-10-17 19:20:44 -0300

    component de grupos para dashborad

---
+++ src/components/Grupos.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Card, Col } from 'react-bootstrap';
+
+export default function Quadros (){
+    return (
+        <>
+        <Card className="border" style={{backgroundColor:'#F3F3F3', marginLeft: '50px',borderRadius: '11px',height: '50rem',width: "14rem", textAlign: "center" }}>
+            {/* fazer um component de outros cards que entrem */}
+        Grupos
+        </Card>
+        </>
+    );
+}
