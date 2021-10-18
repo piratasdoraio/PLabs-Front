071824a7f478ee76b6c9df19b0af3855569c2d4f
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2021-10-18 15:06:42 -0300

    map de botões que irá referenciar "quadros" futuramente

--- src/components/Grupos.tsx
+++ src/components/Grupos.tsx
@@ -1,12 +1,82 @@
-import React from 'react'
+import Button from '@restart/ui/esm/Button';
+import React, {Component} from 'react'
 import { Card, Col } from 'react-bootstrap';
+import CardHeader from 'react-bootstrap/esm/CardHeader';  
+import Board from 'react-trello';
 
-export default function Quadros (){
+//entrar nome do grupo e quadros desse grupo pra ser reproduzido individual varias vezes em quadros
+
+function Cuçeta(){
+  return (
+    <div>doga</div>
+  )
+}
+const grupos = {
+
+}
+const data = {
+    lanes: [
+      {
+        id: 'Coluna1',
+        title: 'Fase 1',
+        label: '2/2',
+        cards: [
+          {
+            id: 'Card1',
+            title: 'Jose Cleitin',
+            description: 'Vendedor de fruta',
+            label: 'Alta prioridade',
+            draggable: false,
+          },
+          {
+            id: 'Card2',
+            title: 'Roberto',
+            description: 'Designer',
+            label: 'Deus',
+            metadata: { sha: 'be312a1' },
+          },
+        ],
+      },
+      {
+        id: 'Coluna2',
+        title: 'Fase2',
+        label: '0/0',
+        cards: [],
+      },
+      {
+        id: 'Coluna2',
+        title: 'Fase2',
+        label: '0/0',
+        cards: [],
+      },
+      {
+        id: 'Coluna2',
+        title: 'Fase2',
+        label: '0/0',
+        cards: [],
+      },
+      {
+        id: 'Coluna2',
+        title: 'Fase2',
+        label: '0/0',
+        cards: [],
+      },
+    ],
+  };
+export default function CardGrupos (){
     return (
         <>
-        <Card className="border" style={{backgroundColor:'#F3F3F3', marginLeft: '50px',borderRadius: '11px',height: '50rem',width: "14rem", textAlign: "center" }}>
-            {/* fazer um component de outros cards que entrem */}
-        Grupos
+        <Card style ={{borderRadius: '15px', paddingBottom: '11px', backgroundColor: '#D2D2D2'}}>
+          <CardHeader>
+            Grupo zureta
+          </CardHeader>
+          {/* listar esses botoes ae */}
+          {data.lanes.map(lanes => (
+            
+            <Button style ={{ textAlign: "left",borderRadius: '20px', marginTop: '5px', marginLeft: '10px', marginRight: '10px' }}>
+            {lanes.id}
+            </Button>
+          ))}
         </Card>
         </>
     );
