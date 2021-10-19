23b60fe02c770fea56538c69b81697414147d7df
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2021-10-18 23:23:25 -0300

    Atualizar tabelas quando alterna-las

--- src/pages/Quadros.tsx
+++ src/pages/Quadros.tsx
@@ -5,7 +5,32 @@ import { Col, Row, Card } from 'react-bootstrap';
 import CardGrupos from '../components/Grupos';
 import CardHeader from 'react-bootstrap/esm/CardHeader';
 
-const data = {
+let grupos = {
+  grupos: [{
+    id: '0',
+    nome: 'Grupo enquadro',
+    quadros: [
+      {
+        id: '0',
+        nome: 'Quadro tubaina'
+      },
+      {
+        id: '1',
+        nome: 'Quadro buteco'
+      }
+    ],
+  },
+  {
+    id: '1',
+    nome: 'Grupo carros 😎',
+    quadros: [{
+      id: '3',
+      nome: 'Quadro do vin disel'
+    }],
+  }
+  ]
+}
+let data = {
   lanes: [
     {
       id: 'Coluna1',
@@ -37,11 +62,113 @@ const data = {
   ],
 };
 
-export default function quadros() {
-  return (
+let eventBus : any = undefined
+const setEventBus = (handle : any) => {
+  eventBus = handle
+}
+
+export function setData(quadroId : string){
+  if (quadroId == "0"){
+    let data = {
+      lanes: [
+        {
+          id: 'Coluna1',
+          title: 'Fase 1',
+          label: '2/2',
+          cards: [
+            {
+              id: 'Card1',
+              title: 'Jose Cleitin',
+              description: 'Vendedor de fruta',
+              label: 'Alta prioridade',
+              draggable: false,
+            },
+            {
+              id: 'Card2',
+              title: 'Roberto',
+              description: 'Designer',
+              label: 'Deus',
+              metadata: { sha: 'be312a1' },
+            },
+          ],
+        },
+        {
+          id: 'Coluna2',
+          title: 'Fase2',
+          label: '0/0',
+          cards: [],
+        },
+      ],
+    };
+    eventBus.publish({type: 'UPDATE_LANES', lanes: data.lanes})
+  }else if (quadroId == '1'){
+    let data = {
+      lanes: [
+        {
+          id: '1',
+          title: 'Carambolas fase 2 :O',
+          label: '2/2',
+          cards: [
+            {
+              id: 'Card1',
+              title: 'Sabedoria',
+              description: 'Musica boa',
+              label: 'Alta prioridade',
+              draggable: false,
+            },
+          ],
+        },
+      ],
+    };
+    eventBus.publish({type: 'UPDATE_LANES', lanes: data.lanes})
+  } else {
+    let data = {
+      lanes: [
+        {
+          id: '1',
+          title: 'Carambolas fase 2 :O',
+          label: '2/2',
+          cards: [
+            {
+              id: 'Card1',
+              title: 'Sabedoria',
+              description: 'Musica boa',
+              label: 'Alta prioridade',
+              draggable: false,
+            },
+            {
+              id: 'Larica',
+              title: 'Larica',
+              label: '0/0',
+              cards: [],
+            },
+             {
+              id: 'Cansado',
+              title: 'Cansado',
+              label: '0/0',
+              cards: [
+                {
+                  id: 'Card1',
+                  title: 'Escrever',
+                  description: 'Notbook',
+                  label: 'Muito baixa prioridade',
+                  draggable: false,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+    eventBus.publish({type: 'UPDATE_LANES', lanes: data.lanes})
+  }
+  return data;
+}
+
+export default function Quadros() {
+  return (  
     <>
       <Base>
-
         <nav style={{ height: '56px', backgroundColor: '#2D4354', marginBottom: '15px' }}>
           <Row>
           <Col style={{ textAlign: "center",marginTop: '14px', color: '#FFF' }}>
@@ -49,18 +176,20 @@ export default function quadros() {
           </Col>
           </Row>
         </nav>
-
         <Row>
-
+          
           <Card className="border" style={{ backgroundColor: '#F3F3F3', marginLeft: '40px', borderRadius: '11px', height: '50rem', width: "20rem"}}>
             {/* fazer um component de outros cards que entrem */}
             <div style={{textAlign: "center" }}>
             Grupos
             </div>
-            <CardGrupos/>
+            {grupos.grupos.map(grupo =>(
+              <CardGrupos grupos={grupo}/>
+            ))}
+            
           </Card>
           <Col>
-            <Board  style={{ backgroundColor: '#F3F3F3' }} data={data} />
+            <Board eventBusHandle={setEventBus} style={{ backgroundColor: '#F3F3F3' }} data={data} />
           </Col>
         </Row>
       </Base>
