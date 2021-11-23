dc7768cdfff10f275f05bbe52b35842db7a82dd0
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2021-11-23 20:11:09 -0300

    XD
    Co-authored-by: raiomarco <raiomarco@users.noreply.github.com>

--- src/components/Grupos.tsx
+++ src/components/Grupos.tsx
@@ -3,7 +3,8 @@ import React, {Component} from 'react'
 import { Card, Col } from 'react-bootstrap';
 import CardHeader from 'react-bootstrap/esm/CardHeader';  
 import Board from 'react-trello';
-import Quadros, {setData} from '../pages/Quadros';
+import Quadros/*, {setData}*/ from '../pages/Quadros';
+import './style.css';
 
   type grupo = {
     grupos:{
@@ -13,20 +14,27 @@ import Quadros, {setData} from '../pages/Quadros';
         id:string,
         nome:string
       }[];
-    }
+    },
+    grupoIndex: number,
+    changeBoard: (grupoIndex: number, quadroIndex: number) => void,
   }
+  //'#D2D2D2'
 export default function CardGrupos (grupo : grupo){
     return (
         <>
-        <Card style ={{borderRadius: '15px', paddingBottom: '11px',marginTop: '11px', backgroundColor: '#D2D2D2'}}>
+        <Card className = "border-0 larica" style = {{borderRadius: '15px', paddingBottom: '11px',marginTop: '11px', backgroundColor: '#FFFF', boxShadow: "0px 0px 15px rgba(69, 69, 69, 0.2)"}}>
           <CardHeader>
             {grupo.grupos.nome}
           </CardHeader>
           {/* listar esses botoes ae */}
-          {grupo.grupos.quadros.map(quadro => (
-            <Button onClick = {() => setData(quadro.id)} style ={{ textAlign: "left",borderRadius: '20px', marginTop: '5px', marginLeft: '10px', marginRight: '10px' }}>
+          {grupo.grupos.quadros.map((quadro, i) => (
+            <div>
+              <Button className='border' onClick = {() => grupo.changeBoard(grupo.grupoIndex, i)} style ={{ width:'85%', textAlign: "left",borderRadius: '20px', marginTop: '5px', marginLeft: '10px', marginRight: '10px' }}>
             {quadro.nome}
             </Button>
+            <Button className='border-0' style={{background: '#ff5262',borderRadius: '40px', position:'relative', right:'5px' , top:'0px'}}><a style = {{color:'#FFF'}}> x </a></Button>
+            </div>
+            
           ))}
         </Card>
         </>
--- src/pages/Quadros.tsx
+++ src/pages/Quadros.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Board from 'react-trello';
 import Base from './BasePage';
 import { Col, Row, Card } from 'react-bootstrap';
 import CardGrupos from '../components/Grupos';
 import CardHeader from 'react-bootstrap/esm/CardHeader';
+import './style.css';
 
 let grupos = {
   grupos: [
@@ -14,10 +15,55 @@ let grupos = {
         {
           id: '0',
           nome: 'Quadro candidatos',
+          lanes: [
+            {
+              id: 'Coluna1',
+              title: 'Fase 1',
+              label: '2/2',
+              cards: [
+                {
+                  id: 'Card1',
+                  title: 'Jose',
+                  description: 'Vendedor de fruta',
+                  label: 'Alta prioridade',
+                  draggable: false,
+                },
+                {
+                  id: 'Card2',
+                  title: 'Roberto',
+                  description: 'Designer',
+                  label: 'Media Prioridade',
+                  metadata: { sha: 'be312a1' },
+                },
+              ],
+            },
+            {
+              id: 'Coluna2',
+              title: 'Fase2',
+              label: '0/0',
+              cards: [],
+            },
+          ],
         },
         {
           id: '1',
           nome: 'Quadro membros',
+          lanes: [
+            {
+              id: '1',
+              title: 'Fase 1',
+              label: '1/1',
+              cards: [
+                {
+                  id: 'Card1',
+                  title: 'Julio',
+                  description: 'Musico',
+                  label: 'Alta prioridade',
+                  draggable: false,
+                },
+              ],
+            },
+          ],
         },
       ],
     },
@@ -28,126 +74,196 @@ let grupos = {
         {
           id: '3',
           nome: 'Quadro candidatos',
+          lanes: [
+            {
+              id: '1',
+              title: 'Fase 1',
+              label: '2/3',
+              cards: [
+                {
+                  id: 'Card1',
+                  title: 'Pedro',
+                  description: 'Programador',
+                  label: 'Extrema prioridade',
+                  draggable: false,
+                },
+              ],
+            },
+          ],
         },
       ],
     },
   ],
 };
-let data = {
-  lanes: [
-    {
-      id: 'Coluna1',
-      title: 'Fase 1',
-      label: '2/2',
-      cards: [
-        {
-          id: 'Card1',
-          title: 'Jose',
-          description: 'Vendedor de fruta',
-          label: 'Alta prioridade',
-          draggable: false,
-        },
-        {
-          id: 'Card2',
-          title: 'Roberto',
-          description: 'Designer',
-          label: 'Media prioridade',
-          metadata: { sha: 'be312a1' },
-        },
-      ],
-    },
-    {
-      id: 'Coluna2',
-      title: 'Fase2',
-      label: '0/0',
-      cards: [],
-    },
-  ],
-};
+
+// let data = {
+//   lanes: [
+//     {
+//       id: 'Coluna1',
+//       title: 'Fase 1',
+//       label: '2/2',
+//       cards: [
+//         {
+//           id: 'Card1',
+//           title: 'Jose',
+//           description: 'Vendedor de fruta',
+//           label: 'Alta prioridade',
+//           draggable: false,
+//         },
+//         {
+//           id: 'Card2',
+//           title: 'Roberto',
+//           description: 'Designer',
+//           label: 'Media prioridade',
+//           metadata: { sha: 'be312a1' },
+//         },
+//       ],
+//     },
+//     {
+//       id: 'Coluna2',
+//       title: 'Fase2',
+//       label: '0/0',
+//       cards: [],
+//     },
+//   ],
+// };
 
 let eventBus: any = undefined;
 const setEventBus = (handle: any) => {
   eventBus = handle;
 };
 
-export function setData(quadroId: string) {
-  if (quadroId == '0') {
-    let data = {
-      lanes: [
-        {
-          id: 'Coluna1',
-          title: 'Fase 1',
-          label: '2/2',
-          cards: [
-            {
-              id: 'Card1',
-              title: 'Jose',
-              description: 'Vendedor de fruta',
-              label: 'Alta prioridade',
-              draggable: false,
-            },
-            {
-              id: 'Card2',
-              title: 'Roberto',
-              description: 'Designer',
-              label: 'Media Prioridade',
-              metadata: { sha: 'be312a1' },
-            },
-          ],
-        },
-        {
-          id: 'Coluna2',
-          title: 'Fase2',
-          label: '0/0',
-          cards: [],
-        },
-      ],
-    };
-    eventBus.publish({ type: 'UPDATE_LANES', lanes: data.lanes });
-  } else if (quadroId == '1') {
-    let data = {
-      lanes: [
-        {
-          id: '1',
-          title: 'Fase 1',
-          label: '1/1',
-          cards: [
-            {
-              id: 'Card1',
-              title: 'Julio',
-              description: 'Musico',
-              label: 'Alta prioridade',
-              draggable: false,
-            },
-          ],
-        },
-      ],
-    };
-    eventBus.publish({ type: 'UPDATE_LANES', lanes: data.lanes });
-  } else {
-    let data = {
-      lanes: [
-        {
-          id: '1',
-          title: 'Fase 1',
-          label: '2/3',
-          cards: [
-            {
-              id: 'Card1',
-              title: 'Pedro',
-              description: 'Programador',
-              label: 'Extrema prioridade',
-              draggable: false,
-            },
-          ],
-        },
-      ],
-    };
-    eventBus.publish({ type: 'UPDATE_LANES', lanes: data.lanes });
-  }
-  return data;
-}
+// export function setData(grupoIndex: number, quadroIndex: number) {
+//   let quadroId = grupoIndex
+//   let data
+
+//   if (quadroId == 0) {
+//     data = {
+//       lanes: [
+//         {
+//           id: 'Coluna1',
+//           title: 'Fase 1',
+//           label: '2/2',
+//           cards: [
+//             {
+//               id: 'Card1',
+//               title: 'Jose',
+//               description: 'Vendedor de fruta',
+//               label: 'Alta prioridade',
+//               draggable: false,
+//             },
+//             {
+//               id: 'Card2',
+//               title: 'Roberto',
+//               description: 'Designer',
+//               label: 'Media Prioridade',
+//               metadata: { sha: 'be312a1' },
+//             },
+//           ],
+//         },
+//         {
+//           id: 'Coluna2',
+//           title: 'Fase2',
+//           label: '0/0',
+//           cards: [],
+//         },
+//       ],
+//     };
+//     eventBus.publish({ type: 'UPDATE_LANES', lanes: data.lanes });
+//   } else if (quadroId == 1) {
+//     data = {
+//       lanes: [
+//         {
+//           id: '1',
+//           title: 'Fase 1',
+//           label: '1/1',
+//           cards: [
+//             {
+//               id: 'Card1',
+//               title: 'Julio',
+//               description: 'Musico',
+//               label: 'Alta prioridade',
+//               draggable: false,
+//             },
+//           ],
+//         },
+//       ],
+//     };
+//     eventBus.publish({ type: 'UPDATE_LANES', lanes: data.lanes });
+//   } else {
+//     data = {
+//       lanes: [
+//         {
+//           id: '1',
+//           title: 'Fase 1',
+//           label: '2/3',
+//           cards: [
+//             {
+//               id: 'Card1',
+//               title: 'Pedro',
+//               description: 'Programador',
+//               label: 'Extrema prioridade',
+//               draggable: false,
+//             },
+//           ],
+//         },
+//       ],
+//     };
+//     eventBus.publish({ type: 'UPDATE_LANES', lanes: data.lanes });
+//   }
+
+//   return data;
+// }
+
 export default function Quadros() {
+
+  const [newGrupos, setNewGrupos] = React.useState(grupos);
+  const [actualGrupo, setActualGrupo] = React.useState(0);
+  const [actualQuadro, setActualQuadro] = React.useState(0);
+
+  // const [myData, setMyData] = useState(grupos);
+
+  // useEffect(() => {
+  //   const saved = JSON.parse(
+  //     localStorage.getItem("newGrupos") || JSON.stringify(newGrupos)
+  //   );
+  //   setNewGrupos(saved);
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem("newGrupos", JSON.stringify(newGrupos));
+  }, [newGrupos]);
+
+  const createGroup = (nome: string) => {
+    let updateGrupos = {
+      grupos: [
+        ...newGrupos.grupos,
+        {
+          id: newGrupos.grupos.length.toString(),
+          nome: nome,
+          quadros: [],
+        },
+      ],
+    };
+    setNewGrupos(updateGrupos);
+  }
+
+  const changeBoard = (grupoIndex: number, quadroIndex: number) => {
+    setActualGrupo(grupoIndex);
+    setActualQuadro(quadroIndex);
+  }
+
+  const changeData = (e: any) => {
+    // setMyData(e);
+    console.log(e)
+    return;
+    let updateGrupos = newGrupos
+    updateGrupos.grupos[actualGrupo].quadros[actualQuadro].lanes = e.lanes
+
+    setNewGrupos(updateGrupos);
+  }
+
+  console.log(newGrupos.grupos[actualGrupo].quadros[actualQuadro].lanes)
 
   return (
     <>
@@ -173,29 +289,39 @@ export default function Quadros() {
             style={{
               backgroundColor: '#F3F3F3',
               marginLeft: '40px',
+              marginRight: '20px',
               borderRadius: '11px',
               height: '50rem',
               width: '20rem',
+
             }}
           >
             {/* fazer um component de outros cards que entrem */}
             <div style={{ textAlign: 'center' }}>Groups</div>
-            {grupos.grupos.map((grupo) => (
-              <CardGrupos grupos={grupo} />
+            {newGrupos.grupos.map((grupo, i) => (
+              <CardGrupos grupos={grupo} grupoIndex={i} changeBoard={changeBoard} />
             ))}
+
+            <Card className="border-0 larica" onClick={() => createGroup(`Grupo ${newGrupos.grupos.length}`)} style={{ cursor: 'pointer', borderRadius: '15px', paddingBottom: '11px', marginTop: '11px', backgroundColor: '#FFFF', boxShadow: "0px 0px 15px rgba(69, 69, 69, 0.2)" }}>
+              <Card.Title></Card.Title>
+              <Card.Subtitle style={{ marginLeft: '10px' }} className="text-muted">add new group</Card.Subtitle>
+            </Card>
           </Card>
-          <Col>
+
+          <Col style={{ backgroundColor: '#F3F3F3',overflowX: "auto", marginRight: '40px', boxShadow: "0px 0px 15px rgba(69, 69, 69, 0.3)", scrollBehavior: "smooth" }}>
             <Board
               eventBusHandle={setEventBus}
-              style={{ backgroundColor: '#F3F3F3' }}
-              data={data}
+              style={{ height: '50rem', backgroundColor: '#F3F3F3',overflowY: "auto" }}
+              data={newGrupos.grupos[actualGrupo].quadros[actualQuadro]}
               draggable
               editable
               canAddLanes
-              addCardTitle='asdsad'
-              //canRemoveLanes
+              //addCardTitle='asdsad'
+              onDataChange={changeData}
+              labelStyle={{ background: "#348954" }}
+            //canRemoveLanes
             />
-          
+
           </Col>
         </Row>
       </Base>
